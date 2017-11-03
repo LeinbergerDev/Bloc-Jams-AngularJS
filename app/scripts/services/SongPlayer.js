34f084ea8cbdd3ed1e813f032dc3e65c27978d51
Jason Leinberger <jason@leinbergerdev.com>
2017-11-03 13:54:23 -0500

    assignment-7-services-2: complete

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,14 +1,40 @@
 (function() {
   function SongPlayer() {
+    /**
+    * @desc SongPlayer object
+    * @type {Object} SongPlayer
+    */
     var SongPlayer = {};
 
+    /**
+    * @desc the selected song.
+    * @type {Object} currentSong
+    */
     var currentSong = null;
+
+    /**
+    * @desc Buzz object audio file
+    * @type {Object}
+    */
     var currentBuzzObject = null;
 
+    /**
+    * @function playSong
+    * @desc SongPlayer object
+    */
+    var playSong = function() {
+      currentBuzzObject.play();
+      song.playing = true;
+    }
+
+    /**
+    * @function setSong
+    * @desc Stops currently playing song and loads new audio file as currentBuzzObject
+    * @param {Object} song
+    */
     var setSong = function(song) {
       if (currentBuzzObject) {
-        currentBuzzObject.stop();
-        currentSong.playing = null;
+        playSong();
       }
       currentBuzzObject = new buzz.sound(song.audioUrl, {
         formats: ['mp3'],
@@ -17,15 +43,21 @@
       currentSong = song;
     };
 
-
+    /**
+    * @desc public method to play selected song
+    * @param {Object} song
+    */
     SongPlayer.play = function(song) {
       if (currentSong !== song){
         setSong(song);
-        currentBuzzObject.play();
-        song.playing = true;
+        playSong();
       }
     };
 
+    /**
+    * @desc public method to pause selected song
+    * @param {Object} song
+    */
     SongPlayer.pause = function(song) {
       currentBuzzObject.pause();
       song.playing = false;
